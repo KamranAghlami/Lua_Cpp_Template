5d7338feb62e20c7daaf0dfcb03c8aedc9482fe0
KamranAghlami <oops.legendary@gmail.com>
2022-07-13 15:43:32 +0430

    .

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    Lua::get().run_string("print(\"Hello, Lua!\")")
+    Lua::get().run_string("print(\"Hello, Lua!\")");
 
     return Lua::get().run_script("../src/lua/main.lua");
 }
